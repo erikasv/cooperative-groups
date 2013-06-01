50d1629c71e1d7e65ff7920db1428b93bde4fb18
Erika Suárez Valencia <marvinesv@gmail.com>
2013-06-01 16:11:28 -0500

    No los subio antes ¬¬

--- cromosoma.rb
+++ cromosoma.rb
@@ -1,6 +1,22 @@
 class Cromosoma
 	
-	def initialize val
-		@val=val
+	def initialize
+		@genRandom=Random.new
+		@val=@genRandom.rand
 	end
+	
+	def mutar
+		@val=@genRandom.rand #Considerara como sería agregar ruido en lugar de cambiarlo, pero creo que así está bien
+	end
+	
+	def aptitud
+		#true si coopera, false si traiciona
+		if @val > 0.5
+			return 1
+		else
+			return 0
+		end
+	end
+	
+	attr_reader :val
 end
