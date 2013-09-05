4ce8b1a9b715cfe522302a32683dc209dddec606
Erika Suárez Valencia <erikasv@localhost.localdomain>
2013-09-05 14:06:49 -0500

    Intentando implementar, me echaron por papas :@

---
+++ agentes/ambiente.rb
@@ -0,0 +1,13 @@
+class Environment
+	@@grid
+	
+	def initialize width, gap, minPlants
+		@plants=minPlants
+		@gapPatch=gap
+		@widthPatch=width
+		
+		#Determinar el lado de la grilla
+		numPatchs= #INCOMPLETO!!!!!!
+		end
+	end
+end
