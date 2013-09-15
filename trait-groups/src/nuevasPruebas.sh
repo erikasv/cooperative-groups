c75e17b9313292fa1a7a4bf09e0ebf0f2d91f682
Erika Suárez Valencia <marvinesv@gmail.com>
2013-09-15 00:17:32 -0500

    Nuevas pruebas

--- trait-groups/src/nuevasPruebas.sh
+++ trait-groups/src/nuevasPruebas.sh
@@ -2,21 +2,21 @@ ruby analisis.rb 100 100 1
 ruby analisis.rb 100 100 2
 ruby analisis.rb 100 100 3
 
-mkdir ../nuevas_100_100
+#mkdir ../nuevas_100_100
 mv *txt *svg ../nuevas_100_100
 
 ruby analisis.rb 100 1000 1
 ruby analisis.rb 100 1000 2
 ruby analisis.rb 100 1000 3
 
-mkdir ../nuevas_100_1000
+#mkdir ../nuevas_100_1000
 mv *txt *svg ../nuevas_100_1000
 
 ruby analisis.rb 1000 1000 1
 ruby analisis.rb 1000 1000 2
 ruby analisis.rb 1000 1000 3
 
-mkdir ../nuevas_1000_1000
+#mkdir ../nuevas_1000_1000
 mv *txt *svg ../nuevas_1000_1000
 
 cd ../..
