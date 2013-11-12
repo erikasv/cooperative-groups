150b04a5db7fa28bcff4ac050ff1bd211108a885
Erika Suárez Valencia <marvinesv@gmail.com>
2013-11-12 14:34:19 -0500

    Pequeña corrección

--- src/agents/executeModel.rb
+++ src/agents/executeModel.rb
@@ -5,9 +5,11 @@ require 'assortment'
 10.times do |execTime|
 	model=Model.new execTime
 	model.run 10000
+	model=nil
 end
 
 10.times do |execTime|
 	assortment=Assortment.new execTime, 10000
 	assortment.meassureAssortment
+	assortment=nil
 end
