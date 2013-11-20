35ef44086041e1d5473ef72c4c91a0ddde3f84fa
Erika Suárez Valencia <marvinesv@gmail.com>
2013-11-19 21:29:23 -0500

    implementada la funcion del analisis completo, falta probar

--- src/agents/analysis.rb
+++ src/agents/analysis.rb
@@ -17,47 +17,86 @@ class Analysis
 		return connection
 	end
 	
-	def calculateGraphicValues
-		@assortment=Array.new
-		@plusOneSD=Array.new
-		@minusOneSD=Array.new
-		
-		for time in 0..@timeUnits do
-			documents=@mongoDB.findAll "assortment", {'timeUnit' => time}
-			
+	def calculateAssortment
+		@assortmentMatrix=Array.new
+		for timeUnit in 0..@timeUnits do
+			cursor=@mongoDB.findAll "assortment", {'timeUnit' => timeUnit}
+			cursor.sort({'executionTime'=>1})
 			xVar=Array.new
-			documents.each{
+			
+			cursor.each{
 				|doc|
 				xVar << doc["assortment"].to_f
 			}
 			
-			average=Statistics.expectedValue xVar
-			standarDeviation=Statistics.standarDeviation xVar
-			
-			@assortment << average
-			@plusOneSD << average+standarDeviation
-			@minusOneSD << average-standarDeviation
-			
-			#~ @mongoDB.writeDataGraphics time, average, average+standarDeviation, average-standarDeviation
+			@assortmentMatrix << xVar 
 		end
 	end
 	
 	def graphicAssortment fileName
+		assortment=Array.new
+		plusOneSD=Array.new
+		minusOneSD=Array.new
+		
+		calculateAssortment
+		
+		@assortmentMatrix.each do |xVar|
+			
+			average=Statistics.expectedValue xVar
+			standarDeviation=Statistics.standarDeviation xVar
+			
+			assortment << average
+			plusOneSD << average+standarDeviation
+			minusOneSD << average-standarDeviation
+			
+			#~ @mongoDB.writeDataGraphics time, average, average+standarDeviation, average-standarDeviation
+		end
+		makeChart assortment, plusOneSD, minusOneSD, fileName
+	end
+	
+	def makeChart assortment , plusOneSD , minusOneSD , fileName 
 		yValues=['assortment', 'plusOneSD', 'minusOneSD']
 		data=Array.new
 		
 		@assortment.each_index{
 			|idx|
-			data << OpenStruct.new({ timeUnit: idx, assortment: @assortment[idx], plusOneSD: @plusOneSD[idx], minusOneSD: @minusOneSD[idx] })
+			data << OpenStruct.new({ timeUnit: idx, assortment: assortment[idx], plusOneSD: plusOneSD[idx], minusOneSD: minusOneSD[idx] })
 		}
 		
 		Graphic.makeLineChart @timeUnits, 1, yValues, data, fileName, 'Unidades de tiempo (generaciones)', 'Assortment'
 	end
 	
-	def calculateDefinitiveValues
+	def graphicAnalysis filename
 		#ra=r-rs=assortment - g-1/N-1
+		
+		assortment=Array.new
+		plusOneSD=Array.new
+		minusOneSD=Array.new
+		
+		calculateAssortment
+		cursor = @mongoDB.findAll "animals",{'executionTime'=>0,'timeUnit'=>0}
+		n = cursor.count()
+		
+		for timeUnit in 0..@timeUnits do
+			xVar = Array.new
+			for executionTime in 0..@executionTimes do
+				cursor = @mongoDB.findAll "dataGroups",{'executionTime'=>executionTime,'timeUnit'=>timeUnit}
+				g = cursor.count()
+				
+				ra = @assortmentMatrix[timeUnit][executionTime] - (( g- 1) /(n-1))
+				xVar << ra
+			end
+			
+			
+			average=Statistics.expectedValue xVar
+			standarDeviation=Statistics.standarDeviation xVar
+			
+			assortment << average
+			plusOneSD << average+standarDeviation
+			minusOneSD << average-standarDeviation
+			
+		end
+		makeChart assortment, plusOneSD, minusOneSD, fileName
 	end
 	
-	def graphicDefinitiveValues
-	end
 end
--- src/agents/executeModel.rb
+++ src/agents/executeModel.rb
@@ -20,5 +20,4 @@ require 'analysis'
 # analysis.graphicAssortment "graphic_assortment_10000_Gw-Ga.svg"
 
 analysis=Analysis.new 10, 10000
-analysis.calculateDefinitiveValues
 analysis.graphicAssortment "graphic_assortment_10000_Gw-Ga.svg"
